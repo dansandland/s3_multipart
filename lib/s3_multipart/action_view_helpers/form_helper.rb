fcd5d7f7df77f0b0427b941d172f8a7e87288372
Junaid Malik <junaid.malik@confiz.com>
2014-06-25 20:41:37 +0500

    Fixed syntax error.

--- lib/s3_multipart/action_view_helpers/form_helper.rb
+++ lib/s3_multipart/action_view_helpers/form_helper.rb
@@ -3,7 +3,7 @@ module S3Multipart
     module FormHelper
       def multipart_uploader_form(options = {})
         uploader_digest = S3Multipart::Uploader.serialize(options[:uploader])
-        html = file_field_tag options[:input_name], :multiple => 'multiple', :data => {:uploader => uploader_digest:, :base_path => options[:base_path]}
+        html = file_field_tag options[:input_name], :multiple => 'multiple', :data => {:uploader => uploader_digest, :base_path => options[:base_path]}
         # html = file_field_tag options[:input_name], :accept => options[:types].join(','), :multiple => 'multiple', :data => {:uploader => uploader_digest}
         html << options[:html].html_safe
         html << button_tag(:class => options[:button_class]) do
